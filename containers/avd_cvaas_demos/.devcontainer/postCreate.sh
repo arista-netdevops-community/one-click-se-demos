88eb002436dcdd750bb2c11bee733b664fb66fff
ankudinov <petr.ankudinov@gmail.com>
2024-03-13 17:58:27 +0100

    fix postCreate

--- containers/avd_cvaas_demos/.devcontainer/postCreate.sh
+++ containers/avd_cvaas_demos/.devcontainer/postCreate.sh
@@ -3,4 +3,5 @@
 set +e
 
 CVTOKEN=$(curl -H "Authorization: Bearer ${CV_API_TOKEN}" "https://www.cv-staging.corp.arista.io/api/v3/services/admin.Enrollment/AddEnrollmentToken" -d '{"enrollmentToken":{"reenrollDevices":["*"],"validFor":"24h"}}' | sed -n 's|.*"token":"\([^"]*\)".*|\1|p')
-echo "$CVTOKEN" > /home/${USERNAME}/tmp/cv-onboarding-token
+echo "$CVTOKEN" > ${CONTAINERWSF}/clab/cv-onboarding-token
+ardl get eos --image-type cEOS --version ${CEOS_LAB_VERSION}  --import-docker
