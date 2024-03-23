44aec43adab7db22ced552446391de14401f9893
pa <pa@arista.com>
2024-03-23 09:23:00 +0000

    fix echo

--- .cc/hooks/post_gen_project.sh
+++ .cc/hooks/post_gen_project.sh
@@ -7,7 +7,7 @@ rm -rf /workspaces/one-click-se-demos/{{ cookiecutter.demo_name }}
 echo "The required files for {{ cookiecutter.demo_name }} were generated."
 echo "Please review them before committing."
 echo ""
-echo "If you are creating a new demo, please do not forget to reference it in the `nav` section of `mkdocs.yml`"
+echo "If you are creating a new demo, please do not forget to reference it in the 'nav' section of 'mkdocs.yml'"
 echo ""
 echo "Example:"
 echo ""
