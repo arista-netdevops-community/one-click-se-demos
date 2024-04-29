5631404044c49fbae6ddf8a34e5d46e7ace62e2c
pa <pa@arista.com>
2024-04-29 21:53:45 +0000

    add more aliases

--- containers/avd-avd-extended-workshop--part-1/.devcontainer/addAliases.sh
+++ containers/avd-avd-extended-workshop--part-1/.devcontainer/addAliases.sh
@@ -3,5 +3,14 @@
 set +e
 
 
+echo "alias dc1-spine1='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.16.1.11'" >> ~/.zshrc
+echo "alias dc1-spine2='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.16.1.12'" >> ~/.zshrc
+echo "alias dc1-leaf1a='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.16.1.101'" >> ~/.zshrc
+echo "alias dc1-leaf1b='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.16.1.102'" >> ~/.zshrc
+echo "alias dc1-leaf2a='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.16.1.103'" >> ~/.zshrc
+echo "alias dc1-leaf2b='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.16.1.104'" >> ~/.zshrc
+echo "alias dc1-leaf1c='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.16.1.151'" >> ~/.zshrc
+echo "alias dc1-leaf2c='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.16.1.152'" >> ~/.zshrc
+
 echo "alias dc1-leaf1-server1='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.16.1.201'" >> ~/.zshrc
 echo "alias dc1-leaf2-server1='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.16.1.202'" >> ~/.zshrc
