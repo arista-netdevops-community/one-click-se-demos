4f6ca61cba1fc08de6ab9e6a23811c49d438946f
pa <pa@arista.com>
2024-04-29 21:20:24 +0000

    add aliases

--- containers/avd-avd-extended-workshop--part-1/.devcontainer/postCreate.sh
+++ containers/avd-avd-extended-workshop--part-1/.devcontainer/postCreate.sh
@@ -6,6 +6,10 @@ CVTOKEN=$(curl -H "Authorization: Bearer ${CV_API_TOKEN}" "https://www.cv-stagin
 echo "$CVTOKEN" > ${CONTAINERWSF}/clab/cv-onboarding-token
 ardl get eos --image-type cEOS --version ${CEOS_LAB_VERSION}  --import-docker
 
+
+echo "alias dc1-leaf1-server1='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.16.1.201'" > ~/.zshrc
+echo "alias dc1-leaf2-server1='sshpass -p arista ssh -o \"StrictHostKeyChecking no\" arista@172.16.1.202'" > ~/.zshrc
+
 # init demo dir as Git repo if requested for this demo env
 if ${GIT_INIT}; then
   cd ${CONTAINERWSF}
