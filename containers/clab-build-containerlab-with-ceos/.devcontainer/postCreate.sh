f8d28e843e3d95c68c6e3b9b355e84a1d18342f2
pa <pa@arista.com>
2024-05-03 21:28:52 +0000

    move var replacement to onCreate stage

--- containers/clab-build-containerlab-with-ceos/.devcontainer/postCreate.sh
+++ containers/clab-build-containerlab-with-ceos/.devcontainer/postCreate.sh
@@ -4,13 +4,6 @@ set +e
 
 ardl get eos --image-type cEOS --version ${CEOS_LAB_VERSION}  --import-docker
 
-# replace all markdown vars in demo directory
-grep -rl 'https://.*.github.io/one-click-se-demos/' . --exclude-dir .git | xargs sed -i 's/https:\/\/.*.github.io\/one-click-se-demos/https:\/\/'"${GITHUB_REPOSITORY%%/*}"'.github.io\/one-click-se-demos/g'
-grep -rl 'https://github.com/.*/one-click-se-demos' . --exclude-dir .git | xargs sed -i 's/https:\/\/github.com\/.*\/one-click-se-demos/https:\/\/github.com\/'"${GITHUB_REPOSITORY%%/*}"'\/one-click-se-demos/g'
-grep -rl '{{gh.repo_name}}' . --exclude-dir .git | xargs sed -i 's/{{gh.repo_name}}/'"${GITHUB_REPOSITORY##*/}"'/g'
-grep -rl '{{gh.org_name}}' . --exclude-dir .git | xargs sed -i 's/{{gh.org_name}}/'"${GITHUB_REPOSITORY%%/*}"'/g'
-grep -rl '{{gh.repository}}' . --exclude-dir .git | xargs sed -i 's@{{gh.org_name}}@'"${GITHUB_REPOSITORY}"'@g'
-
 # init demo dir as Git repo if requested for this demo env
 if ${GIT_INIT}; then
   cd ${CONTAINERWSF}
