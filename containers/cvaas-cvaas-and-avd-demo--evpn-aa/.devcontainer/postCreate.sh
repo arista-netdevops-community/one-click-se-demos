fe0d867481f9101eb62e968631b7ce5765ce195c
pa <pa@arista.com>
2024-05-03 23:52:40 +0000

    update other postCreate files

--- containers/cvaas-cvaas-and-avd-demo--evpn-aa/.devcontainer/postCreate.sh
+++ containers/cvaas-cvaas-and-avd-demo--evpn-aa/.devcontainer/postCreate.sh
@@ -2,6 +2,13 @@
 
 set +e
 
+# replace all markdown vars in demo directory
+grep -rl 'https://.*.github.io/one-click-se-demos/' . --exclude-dir .git | xargs sed -i 's/https:\/\/.*.github.io\/one-click-se-demos/https:\/\/'"${GITHUB_REPOSITORY%%/*}"'.github.io\/one-click-se-demos/g'
+grep -rl 'https://github.com/.*/one-click-se-demos' . --exclude-dir .git | xargs sed -i 's/https:\/\/github.com\/.*\/one-click-se-demos/https:\/\/github.com\/'"${GITHUB_REPOSITORY%%/*}"'\/one-click-se-demos/g'
+grep -rl '{{gh.repo_name}}' . --exclude-dir .git | xargs sed -i 's/{{gh.repo_name}}/'"${GITHUB_REPOSITORY##*/}"'/g'
+grep -rl '{{gh.org_name}}' . --exclude-dir .git | xargs sed -i 's/{{gh.org_name}}/'"${GITHUB_REPOSITORY%%/*}"'/g'
+grep -rl '{{gh.repository}}' . --exclude-dir .git | xargs sed -i 's@{{gh.org_name}}@'"${GITHUB_REPOSITORY}"'@g'
+
 CVTOKEN=$(curl -H "Authorization: Bearer ${CV_API_TOKEN}" "https://www.cv-staging.corp.arista.io/api/v3/services/admin.Enrollment/AddEnrollmentToken" -d '{"enrollmentToken":{"reenrollDevices":["*"],"validFor":"24h"}}' | sed -n 's|.*"token":"\([^"]*\)".*|\1|p')
 echo "$CVTOKEN" > ${CONTAINERWSF}/clab/cv-onboarding-token
 ardl get eos --image-type cEOS --version ${CEOS_LAB_VERSION}  --import-docker
