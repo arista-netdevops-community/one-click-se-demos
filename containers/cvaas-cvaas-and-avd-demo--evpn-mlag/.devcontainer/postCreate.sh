f059f3cbd20902479d7d2cb4c33e3aa42e83deb5
pa <pa@arista.com>
2024-05-05 19:24:23 +0000

    change safe dir

--- containers/cvaas-cvaas-and-avd-demo--evpn-mlag/.devcontainer/postCreate.sh
+++ containers/cvaas-cvaas-and-avd-demo--evpn-mlag/.devcontainer/postCreate.sh
@@ -14,8 +14,8 @@ ardl get eos --image-type cEOS --version ${CEOS_LAB_VERSION}  --import-docker
 # init demo dir as Git repo if requested for this demo env
 if ${GIT_INIT}; then
   cd ${CONTAINERWSF}
-  git config --global --add safe.directory .
   git init
+  git config --global --add safe.directory ${PWD}
   git add .
   git commit -m "git init"
 fi
