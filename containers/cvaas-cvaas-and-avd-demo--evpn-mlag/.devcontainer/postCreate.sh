bac060e749e38833588718899e259a94ce80a70d
pa <pa@arista.com>
2024-05-05 20:13:11 +0000

    swap name and email

--- containers/cvaas-cvaas-and-avd-demo--evpn-mlag/.devcontainer/postCreate.sh
+++ containers/cvaas-cvaas-and-avd-demo--evpn-mlag/.devcontainer/postCreate.sh
@@ -16,8 +16,8 @@ if ${GIT_INIT}; then
   cd ${CONTAINERWSF}
   git init
   git config --global --add safe.directory ${PWD}
-  if [ -z "$(git config user.name)" ]; then git config user.email "user@one-click.lab"; fi
-  if [ -z "$(git config user.email)" ]; then git config user.name "Lab User"; fi
+  if [ -z "$(git config user.name)" ]; then git config user.name "Lab User"; fi
+  if [ -z "$(git config user.email)" ]; then git config user.email user@one-click.lab; fi
   git add .
   git commit -m "git init"
 fi
