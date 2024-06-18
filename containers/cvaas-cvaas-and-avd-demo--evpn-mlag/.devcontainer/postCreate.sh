50b0c036076a6bb27da9b5457ddfba1c9f42d912
pa <pa@arista.com>
2024-06-18 11:19:25 +0000

    update cvtoken logic

--- containers/cvaas-cvaas-and-avd-demo--evpn-mlag/.devcontainer/postCreate.sh
+++ containers/cvaas-cvaas-and-avd-demo--evpn-mlag/.devcontainer/postCreate.sh
@@ -11,7 +11,11 @@ if [ "${CVURL}" ]; then
   grep -rl 'cv-staging.corp.arista.io' . --exclude-dir .git | xargs sed -i 's@cv-staging.corp.arista.io@'"${CVURL}"'@g'
 fi
 
-CVTOKEN=$(curl -H "Authorization: Bearer ${CV_API_TOKEN}" "https://www.cv-staging.corp.arista.io/api/v3/services/admin.Enrollment/AddEnrollmentToken" -d '{"enrollmentToken":{"reenrollDevices":["*"],"validFor":"24h"}}' | sed -n 's|.*"token":"\([^"]*\)".*|\1|p')
+if [ "${CVURL}" ]; then
+  CVTOKEN=$(curl -H "Authorization: Bearer ${CV_API_TOKEN}" "https://www.${CVURL}/api/v3/services/admin.Enrollment/AddEnrollmentToken" -d '{"enrollmentToken":{"reenrollDevices":["*"],"validFor":"24h"}}' | sed -n 's|.*"token":"\([^"]*\)".*|\1|p')
+else
+  CVTOKEN=$(curl -H "Authorization: Bearer ${CV_API_TOKEN}" "https://www.cv-staging.corp.arista.io/api/v3/services/admin.Enrollment/AddEnrollmentToken" -d '{"enrollmentToken":{"reenrollDevices":["*"],"validFor":"24h"}}' | sed -n 's|.*"token":"\([^"]*\)".*|\1|p')
+fi
 echo "$CVTOKEN" > ${CONTAINERWSF}/clab/cv-onboarding-token
 ardl get eos --image-type cEOS --version ${CEOS_LAB_VERSION}  --import-docker
 
